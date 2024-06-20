96dbb9603a86b0e18d3fe65851cebfa5d5e864f1
Sergii Lysenko <soulwish.ls@gmail.com>
2024-06-19 22:48:26 -0400

    update home page, new design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,52 +1,342 @@
 <script>
 	import '../app.css';
+	export const prerender = true;
 </script>
 
 <svelte:head>
-	<title>SolMastery - Your Gateway to the Solana Ecosystem</title>
+	<title>SolMastery - Your Gateway to Blockchain and the Solana Ecosystem_</title>
 </svelte:head>
 
 <div
-	class="flex min-h-screen flex-col bg-[#0C192B] bg-[url('/bg.webp')] bg-cover bg-center bg-no-repeat"
+	class="flex min-h-screen flex-col bg-[#0C192B] bg-[url('/bg.webp')] bg-contain bg-right-top bg-no-repeat"
 >
-	<main
-		class="mx-auto flex w-full max-w-screen-xl grow flex-col items-center gap-6 px-8 pt-8 lg:flex-row lg:px-32"
-	>
-		<div class="grow">
-			<div class="mb-2 text-center lg:text-left">
-				<img src="/logo.png" alt="SolMastery" class="inline-block w-[100px] lg:w-[178px]" />
+	<main class="flex flex-col items-center">
+		<div class="mx-auto flex w-full max-w-screen-xl items-center gap-[22px] px-8 pt-8">
+			<div class="flex flex-col gap-[22px] md:flex-row md:items-center">
+				<img src="/solmastery.svg" alt="SolMastery" class="w-[295px]" loading="lazy" />
+				<h3 class="font-ibm-plex-sans text-[18px] font-light text-[#FAFAFA] xl:text-[24px]">
+					Your Gateway to Blockchain and the Solana Ecosystem_
+				</h3>
 			</div>
-			<h1 class="text-center font-ibm-plex-mono text-[60px] text-white lg:text-left lg:text-[80px]">
-				SolMastery
-			</h1>
-			<h3
-				class="mb-16 text-center font-ibm-plex-sans text-[30px] font-light text-[#FAFAFA] lg:text-left"
-			>
-				Your Gateway to the Solana Ecosystem
-			</h3>
-			<p class="max-w-[460px] font-ibm-plex-sans text-[30px] text-white">
-				<span class="font-bold text-[#12ECAB]">SolMastery</span> offers an educational bot and app that
-				provides comprehensive theory and practical guidance, including simulators, to ease the learning
-				curve.
+			<img src="/solana_logo.svg" alt="Solana" class="ml-auto hidden w-[183px] md:inline-block" />
+		</div>
+		<div
+			class="mx-auto mb-[65px] mt-[80px] flex w-full max-w-screen-xl flex-col flex-col-reverse items-center gap-[22px] px-8 pt-8 md:flex-row"
+		>
+			<div class="flex flex-col items-start gap-[20px]">
+				<h2 class="max-w-[750px] font-ibm-plex-sans text-[48px] font-bold text-[#FAFAFA]">
+					Easy to use application to dive into blockchain and Solana ecosystem
+				</h2>
+				<p class="max-w-[600px] font-ibm-plex-sans text-[18px] text-white">
+					<span class="font-bold text-[#12ECAB]">SolMastery</span> offers an educational bot and app
+					that provides comprehensive theory and practical guidance, including simulators, to ease the
+					learning curve and help everyone to make the first steps and dive into blockchain fundamentals
+					and Solana ecosystem.
+				</p>
+				<a
+					href="https://t.me/solmasterybot"
+					target="_blank"
+					class="shadow-[0_4px_59px_0_rgba(1,254,132,0.4)] mt-8 inline-block rounded-full bg-[#12ECAB] px-20 py-4 text-center font-ibm-plex-sans text-[24px] font-medium text-[#050505] hover:bg-[#12FCAB]"
+					>Start Now</a
+				>
+			</div>
+			<div class="relative">
+				<img src="/phone.webp" alt="SolMastery" class="mx-auto block w-full max-w-[375px]" />
+				<img
+					src="/tg_icon.svg"
+					alt="Telegram"
+					class="absolute bottom-[-20px] right-[-15px] h-[120px] w-[120px] xl:bottom-[-45px] xl:right-[-45px] xl:h-[190px] xl:w-[190px]"
+					loading="lazy"
+				/>
+			</div>
+		</div>
+		<div class="mb-[80px] w-full bg-[#334753]/20 py-[60px]">
+			<div class="mx-auto flex w-full max-w-screen-xl flex-col items-center px-8">
+				<h4 class="mb-[70px] text-center font-ibm-plex-sans text-[36px] font-bold text-[#fff]">
+					Thinking About Mastering Blockchain?
+				</h4>
+				<div class="flex flex-wrap items-start gap-[25px]">
+					<div
+						class="flex basis-[calc(100%/2-25px)] flex-col items-center gap-[40px] sm:basis-[calc(100%/4-25px)]"
+					>
+						<img src="/icon_1.svg" alt="icon" loading="lazy" class="h-[95px] w-[95px]" />
+						<p class="text-center font-ibm-plex-sans text-[16px] text-[#fafafa] sm:text-[18px]">
+							Interested in crypto but unsure where to start?
+						</p>
+					</div>
+					<div
+						class="flex basis-[calc(100%/2-25px)] flex-col items-center gap-[40px] sm:basis-[calc(100%/4-25px)]"
+					>
+						<img src="/icon_2.svg" alt="icon" loading="lazy" class="h-[95px] w-[95px]" />
+						<p class="text-center font-ibm-plex-sans text-[16px] text-[#fafafa] sm:text-[18px]">
+							Familiar with crypto but looking to gain practical skills?
+						</p>
+					</div>
+					<div
+						class="flex basis-[calc(100%/2-25px)] flex-col items-center gap-[40px] sm:basis-[calc(100%/4-25px)]"
+					>
+						<img src="/icon_3.svg" alt="icon" loading="lazy" class="h-[95px] w-[95px]" />
+						<p class="text-center font-ibm-plex-sans text-[16px] text-[#fafafa] sm:text-[18px]">
+							Want to dive deeper into Solana?
+						</p>
+					</div>
+					<div
+						class="flex basis-[calc(100%/2-25px)] flex-col items-center gap-[40px] sm:basis-[calc(100%/4-25px)]"
+					>
+						<img src="/icon_4.svg" alt="icon" loading="lazy" class="h-[95px] w-[95px]" />
+						<p class="text-center font-ibm-plex-sans text-[16px] text-[#fafafa] sm:text-[18px]">
+							Eager to develop your skills from basic to advanced?
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="mx-auto mb-[75px] flex w-full max-w-screen-xl flex-col items-center px-8">
+			<h4 class="mb-[70px] text-center font-ibm-plex-sans text-[36px] font-bold text-[#fff]">
+				What Can You Achieve with SolMastery?
+			</h4>
+			<div class="flex flex-wrap justify-center gap-[40px]">
+				<div
+					class="flex basis-full flex-col items-center gap-[24px] rounded-[30px] bg-gradient-to-b from-[#01FFB3] to-[#319BE1] p-[3px] md:basis-[calc(100%/2-40px)] lg:basis-[calc(100%/3-40px)]"
+				>
+					<div class="h-full w-full rounded-[27px] bg-[#0C192B] p-[35px]">
+						<h5
+							class="mb-[20px] text-center font-ibm-plex-sans text-[24px] font-bold text-[#fafafa]"
+						>
+							Understanding Blockchain Technologies
+						</h5>
+						<p class="text-center font-ibm-plex-sans text-[18px] text-[#fafafa]">
+							Master blockchain technologies, specifically focusing on the Solana blockchain.
+						</p>
+					</div>
+				</div>
+				<div
+					class="flex basis-full flex-col items-center gap-[24px] rounded-[30px] bg-gradient-to-b from-[#01FFB3] to-[#319BE1] p-[3px] md:basis-[calc(100%/2-40px)] lg:basis-[calc(100%/3-40px)]"
+				>
+					<div class="h-full w-full rounded-[27px] bg-[#0C192B] p-[35px]">
+						<h5
+							class="mb-[20px] text-center font-ibm-plex-sans text-[24px] font-bold text-[#fafafa]"
+						>
+							Exploring Cryptocurrencies and Tokens
+						</h5>
+						<p class="text-center font-ibm-plex-sans text-[18px] text-[#fafafa]">
+							Gain insights into different crypto areas, tokens, and more.
+						</p>
+					</div>
+				</div>
+				<div
+					class="flex basis-full flex-col items-center gap-[24px] rounded-[30px] bg-gradient-to-b from-[#01FFB3] to-[#319BE1] p-[3px] md:basis-[calc(100%/2-40px)] lg:basis-[calc(100%/3-40px)]"
+				>
+					<div class="h-full w-full rounded-[27px] bg-[#0C192B] p-[35px]">
+						<h5
+							class="mb-[20px] text-center font-ibm-plex-sans text-[24px] font-bold text-[#fafafa]"
+						>
+							Navigating the Solana Ecosystem
+						</h5>
+						<p class="text-center font-ibm-plex-sans text-[18px] text-[#fafafa]">
+							Dive into the Solana ecosystem and understand its components.
+						</p>
+					</div>
+				</div>
+				<div
+					class="flex basis-full flex-col items-center gap-[24px] rounded-[30px] bg-gradient-to-b from-[#01FFB3] to-[#319BE1] p-[3px] md:basis-[calc(100%/2-40px)] lg:basis-[calc(100%/3-40px)]"
+				>
+					<div class="h-full w-full rounded-[27px] bg-[#0C192B] p-[35px]">
+						<h5
+							class="mb-[20px] text-center font-ibm-plex-sans text-[24px] font-bold text-[#fafafa]"
+						>
+							Enhancing Security Knowledge
+						</h5>
+						<p class="text-center font-ibm-plex-sans text-[18px] text-[#fafafa]">
+							Learn and reinforce security measures and protocols.
+						</p>
+					</div>
+				</div>
+				<div
+					class="flex basis-full flex-col items-center gap-[24px] rounded-[30px] bg-gradient-to-b from-[#01FFB3] to-[#319BE1] p-[3px] md:basis-[calc(100%/2-40px)] lg:basis-[calc(100%/3-40px)]"
+				>
+					<div class="h-full w-full rounded-[27px] bg-[#0C192B] p-[35px]">
+						<h5
+							class="mb-[20px] text-center font-ibm-plex-sans text-[24px] font-bold text-[#fafafa]"
+						>
+							Starting Development, and Product Creation
+						</h5>
+						<p class="text-center font-ibm-plex-sans text-[18px] text-[#fafafa]">
+							Begin working, developing, coding, and creating products.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="mb-[80px] w-full bg-[#334753]/20 py-[60px]">
+			<div class="mx-auto flex w-full max-w-screen-xl flex-col items-center px-8">
+				<h4
+					class="mb-[70px] max-w-[750px] text-center font-ibm-plex-sans text-[36px] font-bold text-[#fff]"
+				>
+					All About Blockchain and Solana, at Your Own Pace, on Your Preferred Device
+				</h4>
+				<div class="flex flex-col items-start gap-[60px] md:flex-row">
+					<div class="flex basis-4/12 flex-col items-center gap-[30px]">
+						<img
+							src="/icon_5.svg"
+							alt="icon"
+							loading="lazy"
+							class="mb-[20px] h-[200px] w-[200px]"
+						/>
+						<p class="text-center font-ibm-plex-sans text-[24px] font-bold text-[#fafafa]">
+							Experience interactive learning through our bot and app.
+						</p>
+						<p class="text-center font-ibm-plex-sans text-[18px] text-[#fafafa]">
+							Accessible from Phone, Tablet, or Computer: Access learning materials conveniently
+							from your preferred device.
+						</p>
+					</div>
+					<div class="flex basis-4/12 flex-col items-center gap-[30px]">
+						<img
+							src="/icon_6.svg"
+							alt="icon"
+							loading="lazy"
+							class="mb-[20px] h-[200px] w-[200px]"
+						/>
+						<p class="text-center font-ibm-plex-sans text-[24px] font-bold text-[#fafafa]">
+							Learn through text, illustrations and automated tasks that simplify complex concepts
+						</p>
+						<p class="text-center font-ibm-plex-sans text-[18px] text-[#fafafa]">
+							Practical knowledge delivered in straightforward language without unnecessary
+							complexity.
+						</p>
+					</div>
+					<div class="flex basis-4/12 flex-col items-center gap-[30px]">
+						<img
+							src="/icon_7.svg"
+							alt="icon"
+							loading="lazy"
+							class="mb-[20px] h-[200px] w-[200px]"
+						/>
+						<p class="text-center font-ibm-plex-sans text-[24px] font-bold text-[#fafafa]">
+							20 Practical Tasks for Reinforcement
+						</p>
+						<p class="text-center font-ibm-plex-sans text-[18px] text-[#fafafa]">
+							Create Wallets, Perform Transactions, Interact with Contracts and Key Resources
+							Securely: Practice these skills using simulations directly within the app.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="mx-auto mb-[125px] flex w-full max-w-screen-xl flex-col items-center px-8">
+			<h4 class="mb-[25px] text-center font-ibm-plex-sans text-[36px] font-bold text-[#fff]">
+				Modules & Topics
+			</h4>
+			<p class="mb-[60px] text-center font-ibm-plex-sans text-[18px] text-[#fafafa]">
+				Discover essential modules covering blockchain fundamentals, Solana ecosystem exploration,
+				security best practices, project creation, and upcoming advanced topics.
 			</p>
+			<div class="flex flex-wrap justify-center gap-[40px]">
+				<div
+					class="flex basis-full lg:basis-[calc(100%/2-40px)] xl:basis-[calc(100%/3-40px)] flex-col rounded-[30px] bg-[#fff]/10 px-[35px] py-[45px]"
+				>
+					<p class="mb-[20px] font-ibm-plex-sans text-[18px] font-bold text-[#12ECAB]">MODULE 1</p>
+					<h5 class="mb-[15px] font-ibm-plex-sans text-[28px] font-bold text-[#fff]">Intro</h5>
+					<p class="font-ibm-plex-sans text-[18px] text-[#fff]">
+						Learn about blockchain technology, including key terms like cryptography,
+						cryptocurrencies, addresses, wallets, transactions, public/private keys, seed phrases,
+						smart contracts, dApps, decentralization, and web3. Explore the limitations of existing
+						blockchains and how solutions like Layer 1, Layer 2, and sidechains aim to address them.
+						Discover Solana's advantages, such as Proof of Stake, Proof of History, energy
+						efficiency, low transaction costs, and minimal environmental impact.
+					</p>
+				</div>
+				<div
+					class="flex basis-full lg:basis-[calc(100%/2-40px)] xl:basis-[calc(100%/3-40px)] flex-col rounded-[30px] bg-[#fff]/10 px-[35px] py-[45px]"
+				>
+					<p class="mb-[20px] font-ibm-plex-sans text-[18px] font-bold text-[#12ECAB]">MODULE 2</p>
+					<h5 class="mb-[15px] font-ibm-plex-sans text-[28px] font-bold text-[#fff]">
+						Getting Started
+					</h5>
+					<p class="font-ibm-plex-sans text-[18px] text-[#fff]">
+						Explore the fundamentals of Solana blockchain, including Mainnet and Testnet. Learn
+						about wallets, creating them, and obtaining blockchain addresses for secure
+						transactions. Understand transactions in blockchain, focusing on the native token SOL.
+						Differentiate between DEX and CEX trading platforms, and discover the Solana Devnet
+						Faucet for acquiring tokens for testing purposes. Gain insights into other tokens, their
+						purposes, acquisition methods, and token standards like FT and NFT.
+					</p>
+				</div>
+				<div
+					class="flex basis-full lg:basis-[calc(100%/2-40px)] xl:basis-[calc(100%/3-40px)] flex-col rounded-[30px] bg-[#fff]/10 px-[35px] py-[45px]"
+				>
+					<p class="mb-[20px] font-ibm-plex-sans text-[18px] font-bold text-[#12ECAB]">MODULE 3</p>
+					<h5 class="mb-[15px] font-ibm-plex-sans text-[28px] font-bold text-[#fff]">
+						Exploring Ecosystem
+					</h5>
+					<p class="font-ibm-plex-sans text-[18px] text-[#fff]">
+						Explore the Solana ecosystem's foundational elements, including a variety of wallets
+						tailored for desktop and mobile use, encompassing both custodial and non-custodial
+						options. Gain insights into the distinct roles of Decentralized Exchanges (DEX) and
+						Centralized Exchanges (CEX), with infographics illustrating trading volumes and
+						transaction data for prominent DEX platforms. Discover the vibrant NFT marketplaces on
+						Solana and delve into the innovative gaming sector.
+					</p>
+				</div>
+				<div
+					class="flex basis-full lg:basis-[calc(100%/2-40px)] xl:basis-[calc(100%/3-40px)] flex-col rounded-[30px] bg-[#fff]/10 px-[35px] py-[45px]"
+				>
+					<p class="mb-[20px] font-ibm-plex-sans text-[18px] font-bold text-[#12ECAB]">MODULE 4</p>
+					<h5 class="mb-[15px] font-ibm-plex-sans text-[28px] font-bold text-[#fff]">Security</h5>
+					<p class="font-ibm-plex-sans text-[18px] text-[#fff]">
+						Learn essential security practices in blockchain, including managing private keys and
+						seed phrases securely across mobile phones and PCs. Understand how to connect wallets to
+						various dApps and smart contracts safely, while exploring security vulnerabilities in
+						different protocols and applications with real-world examples. Identify common types of
+						scams prevalent in the blockchain space, supported by case studies.
+					</p>
+				</div>
+				<div
+					class="flex basis-full lg:basis-[calc(100%/2-40px)] xl:basis-[calc(100%/3-40px)] flex-col rounded-[30px] bg-[#fff]/10 px-[35px] py-[45px]"
+				>
+					<p class="mb-[20px] font-ibm-plex-sans text-[18px] font-bold text-[#12ECAB]">MODULE 5</p>
+					<h5 class="mb-[15px] font-ibm-plex-sans text-[28px] font-bold text-[#fff]">
+						Where to start
+					</h5>
+					<p class="font-ibm-plex-sans text-[18px] text-[#fff]">
+						Begin your journey into creating projects on Solana by understanding the foundational
+						technology stack and prerequisites. Learn about the essential knowledge needed before
+						starting, including blockchain basics, programming languages like Rust, and familiarity
+						with smart contract development. Explore a curated list of official and non-official
+						resources, courses, and tutorials to deepen your understanding and skills.
+					</p>
+				</div>
+				<div class="flex basis-full lg:basis-[calc(100%/2-40px)] xl:basis-[calc(100%/3-40px)] flex-col rounded-[30px] px-[35px] py-[45px]">
+					<p class="mb-[20px] font-ibm-plex-sans text-[18px] font-bold text-[#12ECAB]">
+						MORE TO COME...
+					</p>
+					<p class="font-ibm-plex-sans text-[18px] text-[#fff]">
+						Explore a comprehensive journey through the world of blockchain with upcoming modules
+						covering advanced topics such as DeFi strategies, tokenomics, blockchain scalability
+						solutions, and the future of Web3 innovations.
+					</p>
+				</div>
+			</div>
+		</div>
+		<div
+			class="mx-auto mb-[75px] flex w-full max-w-screen-xl flex-col items-center gap-[35px] bg-[url('/logo_bg.svg')] bg-contain bg-center bg-no-repeat px-8 py-[69px]"
+		>
+			<img src="/solmastery.svg" alt="SolMastery" class="w-[295px]" loading="lazy" />
+			<h4 class="text-center font-ibm-plex-sans text-[28px] font-bold text-[#fff]">
+				Embark on your journey to Blockchain and Solana with SolMastery
+			</h4>
 			<a
-				href="/SolMastery.pdf"
+				href="https://t.me/solmasterybot"
 				target="_blank"
-				class="mt-8 inline-block w-full rounded-full bg-[#12ECAB] px-20 py-4 text-center font-ibm-plex-sans text-[30px] font-medium text-[#050505] hover:bg-[#12FCAB] lg:w-auto"
+				class="shadow-[0_4px_59px_0_rgba(1,254,132,0.4)] inline-block rounded-full bg-[#12ECAB] px-20 py-4 text-center font-ibm-plex-sans text-[24px] font-medium text-[#050505] hover:bg-[#12FCAB]"
 				>Start Now</a
 			>
 		</div>
-		<div class="">
-			<img
-				src="/phone.webp"
-				alt="SolMastery"
-				class="mx-auto mb-16 mt-16 block w-full max-w-[375px]"
-			/>
-		</div>
 	</main>
 	<footer
-		class="mx-auto my-4 w-full max-w-screen-xl px-6 text-center font-ibm-plex-sans text-xs text-white lg:text-base"
+		class="mx-auto my-16 w-full max-w-screen-xl px-6 text-center font-ibm-plex-sans text-[18px] text-white"
 	>
-		Copyright © 2024 SolMastery. All rights reserved.
+		Copyright © {new Date().getFullYear()} SolMastery.com. All rights reserved.
 	</footer>
 </div>
